1f1187ba3bc5720367d53b7cb50eb96c902e905c
Abdi <abdisileshi123@gmail.com>
2023-12-25 20:42:25 +0300

    first commit

---
+++ day1.js
@@ -0,0 +1,28 @@
+//6.
+let variable1;
+let variable2;
+let variable3;
+let variable4;
+//7.
+let variable12 = 10;
+let variable22 = "Hello";
+let variable32 = true;
+let variable42 = [1, 2, 3];
+//8.
+let firstName = "Abdi";
+let lastName = "Sileshi";
+let maritalStatus = "Single";
+let country = "Ethiopia";
+let age = 20;
+//9.
+let myfirstName = "Abdi",
+  mylastName = "Sileshi",
+  mymaritalStatus = "Single",
+  mycountry = "Ethiopia",
+  myage = 20;
+//10.
+let myAge = 20;
+let yourAge = 39;
+
+console.log("My Age:", myAge);
+console.log("Your Age:", yourAge);
